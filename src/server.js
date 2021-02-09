8fbfbac0be980474f0783a86ba43531c60ec09b7
devDongHYun <ehdgus8725@naver.com>
2021-02-09 23:29:06 +0900

    Setting up Server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
-console.log("Hello");
+import { GraphQLServer } from "graphql-yoga";
+
+
+const PORT = 4000;
+
+const server = new GraphQLServer();
+
+
+server.start({ port: PORT}, () => console.log(`✅ Server running on http://localhost:${PORT}`));
